2c5b11a6d31fe2951dbbdee2cb5e41089c8bb6f2
pranavcmr <pranavcmr@gmail.com>
2026-06-05 21:01:26 +0530

    Added

--- scripts/update_progress.py
+++ scripts/update_progress.py
@@ -6,7 +6,7 @@ from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 
 # --- CONFIGURATION ---
-FRIENDS = ["Pranav_MP","khizer12","khrshtt","theLumberJack79"] 
+FRIENDS = ["Pranav_MP", "khrshtt", "theLumberJack79", "kshitij-0712", "NoobGuy29", "Nobody_22"]
 # ^^^ REPLACE THESE with real LeetCode handles!
 
 JSON_FILE = "frontend/public/stats.json"
